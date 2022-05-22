d2a17f09f38af1e6a157b3cb45a27c4b6ae37d10
lasting <lastingcoder@qq.com>
2022-05-22 14:18:37 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import EccLearning from './pages/TestForDocsify';
+import EccLearning from './pages/TestForDocsifyComponent';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
